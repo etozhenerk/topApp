59493e7275360b0adfcc7d8984b2ca224528e853
Mikhail <etozhenerk@yandex.ru>
2021-11-14 22:58:26 +0300

    add ssr props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,67 @@
+import { GetStaticProps } from "next";
 import React, { useEffect, useState } from "react";
 import { HTag, Ptag, Rating, Tag } from "../components";
 import { Button } from "../components";
 import { withLayout } from "../layout/Layout";
+import axios from "axios";
+import { MenuItem } from "../interfaces/menu.interface";
 
-function Home(): JSX.Element {
-
+function Home({ menu }: HomeProps): JSX.Element {
     const [rating, setRating] = useState<number>(4);
 
     return (
         <>
             <HTag tag="h1">Заголовок</HTag>
-            <Button appearance="primary" className="dsada" arrow="right">Кнопка</Button>
-            <Button appearance="ghost" arrow="down">Кнопка</Button>
+            <Button appearance="primary" className="dsada" arrow="right">
+                Кнопка
+            </Button>
+            <Button appearance="ghost" arrow="down">
+                Кнопка
+            </Button>
             <Ptag size="s">Текст</Ptag>
             <Ptag>Текст</Ptag>
             <Ptag size="l">Текст</Ptag>
-            <Tag size="s" color="ghost">S</Tag>
-            <Tag size="m" color="red">M</Tag>
-            <Tag size="s" color="green">S</Tag>
-            <Tag size="s" color="primary">S</Tag>
-            <Tag size="s" color="gray">S</Tag>
-            <Rating rating={rating} isEditable setRating={setRating}/>
+            <Tag size="s" color="ghost">
+                S
+            </Tag>
+            <Tag size="m" color="red">
+                M
+            </Tag>
+            <Tag size="s" color="green">
+                S
+            </Tag>
+            <Tag size="s" color="primary">
+                S
+            </Tag>
+            <Tag size="s" color="gray">
+                S
+            </Tag>
+            <Rating rating={rating} isEditable setRating={setRating} />
+            <ul>
+                {menu.map((m) => (
+                    <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+                ))}
+            </ul>
         </>
     );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+    const firstCategory = 0;
+    const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find", {
+        firstCategory
+    });
+    return {
+        props: {
+            menu,
+            firstCategory,
+        },
+    };
+};
+
+interface HomeProps extends Record<string,unknown> {
+    menu: MenuItem[];
+    firstCategory: number;
+}
